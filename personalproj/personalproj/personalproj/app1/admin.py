6d58ddd0ecf165b42ad3336a916cbcad96b451d4
Nimsi11 <nimsijohn@gmail.com>
2025-01-22 13:30:02 +0530

    Add files via upload

---
+++ personalproj/personalproj/personalproj/app1/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from app1.models import logintable,usertable,personaltable,educationtable,medicaltable,accounttable,familytable
+admin.site.register(logintable)
+admin.site.register(usertable)
+admin.site.register(personaltable)
+admin.site.register(educationtable)
+admin.site.register(medicaltable)
+admin.site.register(accounttable)
+admin.site.register(familytable)
+# Register your models here.
